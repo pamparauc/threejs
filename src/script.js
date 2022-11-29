feaa44cbacf8f721dfd64ec1740cd7ea2c2f1701
Cristian Pamparau <pamparaucritian0@gmail.com>
2022-11-29 18:45:19 +0200

    wip

--- src/script.js
+++ src/script.js
@@ -2,8 +2,9 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
-import { OBB } from 'three/examples/jsm/math/OBB'
-import Stats from 'three/examples/jsm/libs/stats.module'
+var OrbitControls_1 = require("three/examples/jsm/controls/OrbitControls");
+var stats_module_1 = require("three/examples/jsm/libs/stats.module");
+var OBB_1 = require("three/examples/jsm/math/OBB");
 
 // Debug
 const gui = new dat.GUI()
@@ -13,6 +14,7 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+//scene.add(new THREE.AxesHelper(5));
 
 let focus = 2;
 
@@ -22,33 +24,67 @@ pointLight.position.y = 3
 pointLight.position.z = 4
 scene.add(pointLight)
 
-const geometry1 = new THREE.PlaneGeometry( 2.5, 0.01 );
 
-const material1 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-const plane1 = new THREE.Mesh( geometry1, material1 );
-
+// axa 1
+const geometry1 = new THREE.PlaneGeometry( 2, 0.01 );
+geometry1.computeBoundingBox();
+const material1 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} ); // rosu
+const plane1 = new THREE.Mesh( geometry1, material1);
+plane1.userData.obb = new OBB_1.OBB();
+plane1.geometry.userData.obb = new OBB_1.OBB().fromBox3(plane1.geometry.boundingBox);
+plane1.position.x = -1.95;
+plane1.position.y = 1.2;
 scene.add( plane1 );
-plane1.position.x = 0.7;
-plane1.position.y = -0.1;
 
-const geometry2 = new THREE.PlaneGeometry( 2.5, 0.01 );
-
+// axa 2
+const geometry2 = new THREE.PlaneGeometry( 2, 0.01 );
+geometry2.computeBoundingBox();
 const material2 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
-const plane2 = new THREE.Mesh( geometry2, material2);
-
-//plane2.geometry.userData.obb = new OBB().fromBox3(
- //   plane2.geometry.boundingBox as THREE.Box3
-//)
-//plane2.userData.obb = new OBB()
+const plane2 = new THREE.Mesh( geometry2, material2 );
+plane2.userData.obb = new OBB_1.OBB();
+plane2.geometry.userData.obb = new OBB_1.OBB().fromBox3(plane2.geometry.boundingBox);
+plane2.position.x = -1.5;
+plane2.position.y = .1;
 scene.add( plane2 );
-plane2.position.x = -1;
-plane2.position.y = 1;
+
+// axa 3
+const geometry3 = new THREE.PlaneGeometry( 2, 0.01 );
+geometry3.computeBoundingBox();
+const material3 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const plane3 = new THREE.Mesh( geometry3, material3 );
+plane3.userData.obb = new OBB_1.OBB();
+plane3.geometry.userData.obb = new OBB_1.OBB().fromBox3(plane3.geometry.boundingBox);
+plane3.position.x = 0.3;
+plane3.position.y = .7;
+scene.add( plane3 );
+
+// axa 4
+const geometry4 = new THREE.PlaneGeometry( 2, 0.01 );
+geometry4.computeBoundingBox();
+const material4 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const plane4 = new THREE.Mesh( geometry4, material4 );
+plane4.userData.obb = new OBB_1.OBB();
+plane4.geometry.userData.obb = new OBB_1.OBB().fromBox3(plane4.geometry.boundingBox);
+plane4.position.x = 0.95;
+plane4.position.y = -0.7;
+scene.add( plane4 );
+
+// axa 5
+const geometry5 = new THREE.PlaneGeometry( 2, 0.01 );
+geometry5.computeBoundingBox();
+const material5 = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+const plane5 = new THREE.Mesh( geometry5, material5 );
+plane5.userData.obb = new OBB_1.OBB();
+plane5.geometry.userData.obb = new OBB_1.OBB().fromBox3(plane5.geometry.boundingBox);
+plane5.position.x = 1.99;
+plane5.position.y = 0.3;
+scene.add( plane5 );
 
 
 /**
  * Sizes
  */
-const sizes = {
+ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
@@ -67,6 +103,7 @@ window.addEventListener('resize', () =>
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
+
 
 /**
  * Camera
@@ -90,62 +127,71 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
+var controls = new OrbitControls_1.OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 /**
  * Animate
  */
 
 const clock = new THREE.Clock()
-plane2.scale.y *=2.9;
-function tt(){
-    
-   
-}
-setInterval(tt, 100);
+
+
 const tick = () =>
 {
 
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    plane1.rotation.z = .051 * elapsedTime
+    plane1.rotation.z = .021 * elapsedTime
     plane2.rotation.z = .04 * elapsedTime
+    plane3.rotation.z = .031 * elapsedTime
+    plane4.rotation.z = .034 * elapsedTime
+    plane5.rotation.z = .039 * elapsedTime
 
     // Render
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
-    if (detectCollisionCubes(plane2, plane1)) {
-        console.log(focus);
-        if (focus == 2)
+	plane1.userData.obb.copy(plane1.geometry.userData.obb);
+    plane2.userData.obb.copy(plane2.geometry.userData.obb);
+    plane3.userData.obb.copy(plane3.geometry.userData.obb);
+    plane4.userData.obb.copy(plane4.geometry.userData.obb);
+    plane5.userData.obb.copy(plane5.geometry.userData.obb);
+    plane1.userData.obb.applyMatrix4(plane1.matrixWorld);
+    plane2.userData.obb.applyMatrix4(plane2.matrixWorld);
+    plane3.userData.obb.applyMatrix4(plane3.matrixWorld);
+    plane4.userData.obb.applyMatrix4(plane4.matrixWorld);
+    plane5.userData.obb.applyMatrix4(plane5.matrixWorld);
+    if (plane1.userData.obb.intersectsOBB(plane2.userData.obb)) { // plane 1 era rosu
+        var res = "1 intersectat cu 2";
+        if (plane2.userData.obb.intersectsOBB(plane3.userData.obb))
         {
-            plane2.scale.y/=2.9;
-            plane1.scale.y*=2.9;
-            focus =1;
-            return;
+            res += " SI 2 intersectat cu 3";
+            if (plane3.userData.obb.intersectsOBB(plane4.userData.obb))
+            {
+                res += " SI 3 intersectat cu 4";
+                if (plane4.userData.obb.intersectsOBB(plane5.userData.obb))
+                {
+                    res += " SI 3 intersectat cu 4";
+                }
+            }
         }
-        if (focus == 1)
-        {
-            plane1.scale.y/=2.9;
-            plane2.scale.y*=2.9;
-            focus =2;
-            return;
-        }
+        console.log(res);
     }
+    if (plane2.userData.obb.intersectsOBB(plane3.userData.obb)) { // plane 1 era rosu
+        console.log("2 intersectat cu 3");
+    }
+    if (plane3.userData.obb.intersectsOBB(plane4.userData.obb)) { // plane 1 era rosu
+        console.log("3 intersectat cu 4");
+    }
+    if (plane4.userData.obb.intersectsOBB(plane5.userData.obb)) { // plane 1 era rosu
+        console.log("4 intersectat cu 5");
+    }
+    if (plane3.userData.obb.intersectsOBB(plane5.userData.obb)) { // plane 1 era rosu
+        console.log("3 intersectat cu 5");
+    }
+
 }
-function detectCollisionCubes(object1, object2){
-    object1.geometry.computeBoundingBox(); //not needed if its already calculated
-    object2.geometry.computeBoundingBox();
-    object1.updateMatrixWorld();
-    object2.updateMatrixWorld();
-    
-    var box1 = object1.geometry.boundingBox.clone();
-    box1.applyMatrix4(object1.matrixWorld);
-  
-    var box2 = object2.geometry.boundingBox.clone();
-    box2.applyMatrix4(object2.matrixWorld);
-  
-    return box1.intersectsBox(box2);
-  }
 tick()
+
